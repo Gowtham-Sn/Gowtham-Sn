e2e5b2f372b7c8c94244c10c7116be90d5c744c3
Gowtham-Sn <gowthamtechiee@gmail.com>
2025-01-22 10:10:35 +0530

    Init

--- web/src/main/java/com/Aquatech/web/ContactFormrepository/ContactFormRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Aquatech.web.ContactFormrepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Aquatech.web.Entity.ContactForm;
-
-public interface ContactFormRepository extends JpaRepository<ContactForm, Long> {
-	
-}
-
